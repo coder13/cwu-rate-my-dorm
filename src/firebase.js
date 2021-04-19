212be72576b6d62d0f5d44d9a3480f557c0239e4
Caleb <choover11@gmail.com>
2021-04-19 15:49:14 -0700

    added firebase config

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyA8A93K6mRFUIv7C1qOMffL0c46iQjkcrU',
+  authDomain: 'cwu-rate-my-dorm.firebaseapp.com',
+  projectId: 'cwu-rate-my-dorm',
+  storageBucket: 'cwu-rate-my-dorm.appspot.com',
+  messagingSenderId: '683058081928',
+  appId: '1:683058081928:web:80f474f8c6f343f7e62f3d',
+  measurementId: 'G-DH9FW3QQXF',
+};
+
+firebase.initializeApp(firebaseConfig);
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
