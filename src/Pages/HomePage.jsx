5aced3ed1e6166cc912035fb1233a5b51ea6b5c9
Elijah McCoy <elijahmccoy51@users.noreply.github.com>
2021-04-20 23:43:11 -0700

    Removed React modules for multiple CSS files. Added MainPage JS for when the HTML mounts (where your script goes).

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,58 +1,76 @@
 import React, { Component } from "react";
 import CWUMap from'../Assets/CWU_Campus_Map.jpg'
-import styles from '../Styles/HomePage.module.css';
-import mainStyles from '../Styles/MainTheme.module.css';
+import '../Styles/HomePage.css';
 import TopBarComponent from '../Components/TopBarComponent'
 
 class HomePage extends Component {
+    
+    alerting()
+    {
+        var x = this.textContent;
+        alert(x);
+    }
+
+    //Where your normal JS script goes. This function runs after all the HTML
+    //Has mounted.
+    componentDidMount() {
+
+        var hallButtons = document.getElementsByClassName('listItem');
+
+        for(var i = 0; i < hallButtons.length ; i++)
+        {
+            hallButtons[i].addEventListener('click', this.alerting);
+        }
+
+    }
+    
     render() {
         return (
-            
             <div>
                 <TopBarComponent />
-                <div className = {mainStyles.mainDivSection}>
-                    <div className = {styles.mainSection}>
+                <div className = 'mainDivSection'>
+                    <div className = 'mainSection'>
 
-                        <div className = {styles.listSection}>
-                            <div className = {styles.listContainer}>
+                        <div className = 'listSection'>
+                            <div className = 'listContainer'>
                                
-                                <div className = {styles.listItemWrapper}> 
-                                    <div className = {styles.listItem}>
+                                <div className = 'listItemWrapper'> 
+                                    <div className = 'listItem'>
                                         Kamola Hall
                                     </div>
                                 </div>
-                                <div className = {styles.listItemWrapper}> 
-                                    <div className = {styles.listItem}>
+                                <div className = 'listItemWrapper'> 
+                                    <div className = 'listItem'>
                                         Hall 2
                                     </div>
                                 </div>
-                                <div className = {styles.listItemWrapper}> 
-                                    <div className = {styles.listItem}>
+                                <div className = 'listItemWrapper'> 
+                                    <div className = 'listItem'>
                                         Hall 3
                                     </div>
                                 </div>
-                                <div className = {styles.listItemWrapper}> 
-                                    <div className = {styles.listItem}>
+                                <div className = 'listItemWrapper'> 
+                                    <div className = 'listItem'>
                                         Hall 4
                                     </div>
                                 </div>
-                                <div className = {styles.listItemWrapper}> 
-                                    <div className = {styles.listItem}>
+                                <div className = 'listItemWrapper'> 
+                                    <div className = 'listItem'>
                                         Hall 5
                                     </div>
                                 </div>
-                                <div className = {styles.listItemWrapper}> 
-                                    <div className = {styles.listItem}>
+                                <div className = 'listItemWrapper'> 
+                                    <div className = 'listItem'>
                                         Hall 6
                                     </div>
                                 </div>
-                                <div className = {styles.listItemWrapper}> 
-                                    <div className = {styles.listItem}>
+                                <div className = 'listItemWrapper'> 
+                                    <div className = 'listItem'>
                                         Hall 7
                                     </div>
                                 </div>
-                                <div className = {styles.listItemWrapper}> 
-                                    <div className = {styles.listItem}>
+                                <div className = 'listItemWrapper'> 
+                                    <div className = 'listItem'>
                                         Hall 8
                                     </div>
                                 </div>
@@ -60,10 +78,10 @@ class HomePage extends Component {
                             </div>
                         </div>
 
-                        <div className = {styles.mapSection}>
+                        <div className = 'mapSection'>
 
-                            <div className= {styles.mapContainer}>
-                                <img src = {CWUMap} className = {styles.mapImage} alt = ''/>
+                            <div className= 'mapContainer'>
+                                <img src = {CWUMap} className = 'mapImage' alt = ''/>
                             </div>
 
                         </div>
