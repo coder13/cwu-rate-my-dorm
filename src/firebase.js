eb75204a37fd70f85942d93a5eca61836cb66ed8
Joshua Steiner <joshuajsteiner@gmail.com>
2021-04-21 18:02:20 -0700

    Added function that hosts the retrieving/saving of users data.

--- src/firebase.js
+++ src/firebase.js
@@ -21,3 +21,42 @@ export const firestore = firebase.firestore();
 export const signInWithGoogle = () => {
   auth.signInWithPopup(provider);
 };
+
+function generateUserDocument (user, additionalData) {
+  if (!user) return;
+  export const userRef = firestore.doc(`users/${user.uid}`);
+  export const snapshot = await userRef.get();
+
+}
+
+export const generateUserDocument = async (user, additionalData) => {
+  if (!user) return;
+  export const userRef = firestore.doc(`users/${user.uid}`);
+  export const snapshot = await userRef.get();
+  if (!snapshot.exists) {
+    const { email, displayName, photoURL } = user;
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        photoURL,
+        ...additionalData
+      });
+    } catch (error) {
+      console.error("Error creating user document", error);
+    }
+  }
+  return getUserDocument(user.uid);
+};
+const getUserDocument = async uid => {
+  if (!uid) return null;
+  try {
+    const userDocument = await firestore.doc(`users/${uid}`).get();
+    return {
+      uid,
+      ...userDocument.data()
+    };
+  } catch (error) {
+    console.error("Error fetching user", error);
+  }
+};
