76c1602a8445e2ae90051afe8498c10e11c5813b
Elijah McCoy <elijahmccoy51@users.noreply.github.com>
2021-04-22 17:46:43 -0700

    Made buttons load dynamically. Set up for navigation

--- src/Pages/ExampleHallPage.jsx
+++ src/Pages/ExampleHallPage.jsx
@@ -19,41 +19,59 @@ class ExampleHallPage extends Component {
   {
 
     return(
+
       <div>
+
         <TopBarComponent />
+
         <div className='windowDivSection'>
 
           <div className='sideSection'>
+
             <h1>Side Section 1</h1>
+
           </div>
 
           <div className='middleSection'>
+
             <div className='infoAndReviewSection'>
               
               <div className='informationSection'>
+
                 <div className='amenities'>
+
                   <h1>Amenities</h1>
+
                 </div>
+
                 <div className='imageGallery'>
+
                   <h1>Image Gallery</h1>
+
                 </div>
 
               </div>
 
               <div className='topReviewSection'>
+
                 <h1>Top Review Section</h1>
+
               </div>
 
             </div>
 
             <div className='reviewsSection'>
+
               <h1>Reviews Section</h1>
+
             </div>
 
           </div>
 
           <div className='sideSection'>
+
             <h1>Side Section 2</h1>
+            
           </div>
 
         </div>
