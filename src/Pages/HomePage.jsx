8cbcfc0663e8b5590643e20a6f157d45b9a7319c
Elijah McCoy <elijahmccoy51@users.noreply.github.com>
2021-04-24 23:39:02 -0700

    Queries database for names. Loader added.

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -9,27 +9,18 @@ import { withRouter } from "react-router-dom";
 import CWUMap from '../Assets/CWU_Campus_Map.jpg'
 import '../Styles/HomePage.css';
 import TopBarComponent from '../Components/TopBarComponent'
+import LoaderComponent from '../Components/LoaderComponent'
 
 class HomePage extends Component {
 
   constructor(props) {
     super(props);
 
-    //Set up firebase:
-    //firestore.settings({ timestampsInSnapshots: true });
-
     //Set up state:
     this.state = {
-      hallNamesTest: []
+      hallNames: [],
+      loaded: false
     };
-
-    //Initilize class vars. (Could be loaded by database)
-    this.hallNames = ["Barto Hall", "Beck Hall", "Meisner Hall", "Davies Hall",
-    "Sparks Hall", "Hitchcock Hall", "Quigley Hall", "Wilson Hall",
-    "Alford-Monthomery Hall", "Kennedy Hall", "Green Hall", "Carmody-Munro Hall",
-    "Wendell Hill Hall", "North Hall", "Stephens-Whitney Hall", "Sue Lombard Hall",
-    "Kamola Hall", "Moore Hall", "Dogmore Hall", "Brooklane Village",
-    "Wahle Apartments", "Anderson Apartments", "Student Village"];
 
     //Bind function to class instance.
     this.navigateToPage = this.navigateToPage.bind(this);
@@ -48,7 +39,7 @@ class HomePage extends Component {
     
     //Store listButtons:
     const hallNames = props.hallNames;
-    const listButtonItems = this.state.hallNamesTest.map((hallName) =>
+    const listButtonItems = hallNames.map((hallName) =>
       <div className='listItemWrapper'>
         <div className='listItem' key={hallName.toString()} onClick={() =>{this.navigateToPage(hallName.toString())}}>
           <h1>{hallName}</h1>
@@ -66,65 +57,68 @@ class HomePage extends Component {
   //===componentDidMount===
   //Desc: JS for once the render method is mounted.
   componentDidMount() {
-    //Loading from database. SHOULD BE in didmount?
 
-    var tempList = [];
-
-    const ref = firestore.collection('Dorms');
-    ref.get().then((snapshot) => {
+    //Loading names from database.
+    const db = firestore;
+    db.collection('Dorms').get().then((snapshot) => {
       snapshot.docs.forEach(doc => {
         // name, description, rating, amenities[], images[]
-        this.state.hallNamesTest.push(doc.get('name'));
+        this.setState({ hallNames: [...this.state.hallNames, doc.get('name')] });
       });
     });
 
-    console.log("NAMES", this.state.hallNamesTest);
-    console.log("temp", tempList);
+    //SetState:
+    this.setState({loaded: true});
   }
 
   //===render===
   //Desc: Renders the html.
   render() {
-    console.log("NAMES RENDER", this.state.hallNamesTest);
-    return (
 
-      <div>
+    //Checks if list is < 1 and if list is loaded.
+    if(this.state.loaded && this.state.hallNames.length)
+    {
+      return (
+        <div>
+          
+          <TopBarComponent />
 
-        <TopBarComponent />
+          <div className='mainDivSection'>
 
-        <div className='mainDivSection'>
+            <div className='sideSection'></div>
 
-          <div className='sideSection'></div>
+              <div className='mainSection'>
 
-          <div className='mainSection'>
-
-            <div className='listAndMapSection'>
-
-              <this.buttonList hallNames={this.state.hallNamesTest} />
-
-              <div className='mapSection'>
-
-                <div className='mapContainer'>
-
-                  {/*Could be loaded from database*/}
-                  <img src={CWUMap} className='mapImage' alt='' />
-
+                <div className='listAndMapSection'>
+  
+                  <this.buttonList hallNames={this.state.hallNames} />
+  
+                  <div className='mapSection'>
+  
+                    <div className='mapContainer'>
+  
+                      {/*Could be loaded from database*/}
+                      <img src={CWUMap} className='mapImage' alt='' />
+  
+                    </div>
+  
+                  </div>
+  
                 </div>
-
+  
               </div>
-
-            </div>
-
+  
+            <div className='sideSection'></div>
+  
           </div>
-
-          <div className='sideSection'></div>
-
+  
         </div>
-
-      </div>
-
-    )
-
+      )
+    }
+    else //List is still loading. 
+    {
+      return(<LoaderComponent />);
+    }
   }
 }
 
