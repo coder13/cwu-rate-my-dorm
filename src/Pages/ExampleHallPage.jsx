4a0858a907a3c9fce9a79aaa0477278167e8a4b0
elimccoy <elijmccoy01@gmail.com>
2021-04-28 10:10:05 -0700

    Refactored ExampleHallPage to Bootstrap. Added image scroller.

--- src/Pages/ExampleHallPage.jsx
+++ src/Pages/ExampleHallPage.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
+import {Container, Row, Col, Carousel} from 'react-bootstrap'
 import '../Styles/ExampleHallPage.css';
 import TopBarComponent from '../Components/TopBarComponent'
 
@@ -28,59 +29,81 @@ class ExampleHallPage extends Component {
 
         <TopBarComponent />
 
-        <div className='windowDivSection'>
+        <Container fluid='true' className='windowDivSection'>
+          
+          <Row>
 
-          <div className='sideSection'>
+            <Col className='middleSection'>
 
-            <h1>Side Section 1</h1>
+             <Row className='infoAndTopReviewSection'>
 
-          </div>
-
-          <div className='middleSection'>
-
-            {/*Need to add top bar for hall name.*/}  
-      
-            <div className='infoAndReviewSection'>
-              
-              <div className='informationSection'>
-
-                <div className='amenities'>
-
+              <Col className='titleAndInformationSection'>
+                  
+                <Row className='titleBlock'>
                   <h1>{this.hallName}</h1>
+                </Row>
 
-                </div>
+                  <Row className='imageGalleryBlock'>
 
-                <div className='imageGallery'>
+                    <Carousel className='imageCarousel'>
+                      <Carousel.Item>
+                        <img
+                          className='carouselImageExample'
+                          src="https://www.kpq.com/wp-content/uploads/2018/07/CWU.jpg"
+                          alt="First slide"
+                        />
+                      </Carousel.Item>
 
-                  <h1>Image Gallery</h1>
+                      <Carousel.Item>
+                        <img
+                          className='carouselImageExample'
+                          src="https://katu.com/resources/media/b2bd1ced-1737-478c-92d9-fa0fc374d6a2-large16x9_190419_pio_central_washington_university_cwu.jpg?1555702482536"
+                          alt="First slide"
+                        />
+                      </Carousel.Item>
 
-                </div>
+                      <Carousel.Item>
+                        <img
+                          className='carouselImageExample'
+                          src="https://www.kpq.com/wp-content/uploads/2018/07/CWU.jpg"
+                          alt="First slide"
+                        />
+                      </Carousel.Item>
+                    </Carousel>
 
-              </div>
+                  </Row>
 
-              <div className='topReviewSection'>
+                <Row className='infoBlock'>
+                  <h1>Info</h1>
+                </Row>
 
-                <h1>Top Review Section</h1>
+              </Col>
 
-              </div>
+              <div className ='spacer'></div>
 
-            </div>
+              <Col className='topReviewSection'>
 
-            <div className='reviewsSection'>
+                <Row className='topReviewBlock'>
+                  <h1>Top Reviews Here</h1>
+                </Row>
 
-              <h1>Reviews Section</h1>
+              </Col>
 
-            </div>
+             </Row>
 
-          </div>
+             <Row className='reviewsSection'>
+               
+               <Row className='reviewsBlock'>
+                  <h1>Reviews</h1>
+               </Row>
 
-          <div className='sideSection'>
+             </Row>
 
-            <h1>Side Section 2</h1>
-            
-          </div>
+            </Col>
 
-        </div>
+          </Row>
+
+        </Container>
 
       </div>
     )
