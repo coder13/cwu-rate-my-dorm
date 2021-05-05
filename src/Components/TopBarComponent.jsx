a5a138331beed4e785d2e6639e29c5e15b8323c0
Caleb <choover11@gmail.com>
2021-05-05 12:34:25 -0700

    converted topbarcomponent to 2 spaces

--- src/Components/TopBarComponent.jsx
+++ src/Components/TopBarComponent.jsx
@@ -6,71 +6,69 @@ import { UserContext } from '../providers/UserProvider';
 import { auth } from "../firebase";
 
 class TopBarComponent extends Component {
-    static contextType = UserContext;
+  static contextType = UserContext;
 
-    render() {
-        return (
-            <div className={TopBarCompStyles.topbar}>
-
-                <div className={TopBarCompStyles.SearchBox}>
-
-                    <img className={TopBarCompStyles.CwuLogo} src={CWULogo} alt="logo" />
-                    
-                    <form>
-                        <input className={TopBarCompStyles.searchInput} type="Text" name="search" placeholder="Search Rate My Dorm"/>
-                    </form>
-
-                    <div className={TopBarCompStyles.MenuItemBox}>
-                        <div className={TopBarCompStyles.MenuItem}>Home</div>
-                        <div className={TopBarCompStyles.MenuItem}>Other Sections</div>
-                    </div>
-
-                </div>
+  render() {
+    return (
+      <div className={TopBarCompStyles.topbar}>
+        <div className={TopBarCompStyles.SearchBox}>
+          <img className={TopBarCompStyles.CwuLogo} src={CWULogo} alt="logo" />
             
-                <div className={TopBarCompStyles.signInSection}>
-                    <UserContext.Consumer>
-                        {(user) => (
-                            user
-                            ? (
-                                <>
-                                    <div>{`logged in as ${user.displayName}`}</div>
-                                    <div className={TopBarCompStyles.LogInSignUpButtons} onClick={() => auth.signOut()}>Sign-out</div>
-                                </>
-                            )
-                            : (
-                                <>
-                                    <Link
-                                        className={TopBarCompStyles.LogInSignUpButtons}
-                                        to={{
-                                            pathname: '/signin',
-                                            state: {
-                                                from: document.location.pathname
-                                            }
-                                        }}
-                                        >
-                                        Sign-in
-                                    </Link>
-                                    <Link
-                                        className={TopBarCompStyles.LogInSignUpButtons}
-                                        to={{
-                                            pathname: '/signup',
-                                            state: {
-                                                from: document.location.pathname
-                                            }
-                                        }}
-                                    >
-                                        Sign-up
-                                    </Link>
-                                </>
-                            )
-                        )}
-                    </UserContext.Consumer>
-                    <div className={TopBarCompStyles.LeaveAReviwButton}>Leave a Review</div>
-                </div>
+          <form>
+              <input className={TopBarCompStyles.searchInput} type="Text" name="search" placeholder="Search Rate My Dorm"/>
+          </form>
 
-            </div>    
-        )
-    }
+          <div className={TopBarCompStyles.MenuItemBox}>
+              <div className={TopBarCompStyles.MenuItem}>Home</div>
+              <div className={TopBarCompStyles.MenuItem}>Other Sections</div>
+          </div>
+
+      </div>
+    
+      <div className={TopBarCompStyles.signInSection}>
+          <UserContext.Consumer>
+              {(user) => (
+                user
+                ? (
+                  <>
+                    <div>{`logged in as ${user.displayName}`}</div>
+                    <div className={TopBarCompStyles.LogInSignUpButtons} onClick={() => auth.signOut()}>Sign-out</div>
+                  </>
+                )
+                : (
+                  <>
+                    <Link
+                      className={TopBarCompStyles.LogInSignUpButtons}
+                      to={{
+                        pathname: '/signin',
+                        state: {
+                          from: document.location.pathname
+                        }
+                      }}
+                    >
+                      Sign-in
+                    </Link>
+                    <Link
+                      className={TopBarCompStyles.LogInSignUpButtons}
+                      to={{
+                        pathname: '/signup',
+                        state: {
+                          from: document.location.pathname
+                        }
+                      }}
+                    >
+                      Sign-up
+                    </Link>
+                  </>
+                )
+              )}
+          </UserContext.Consumer>
+          <div className={TopBarCompStyles.LeaveAReviwButton}>Leave a Review</div>
+        </div>
+
+      </div>
+    )
+  }
 }
 
 export default TopBarComponent;
