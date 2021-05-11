6b1598855da0f6dd35448e65b466439d73b593de
elimccoy <elijmccoy01@gmail.com>
2021-05-10 18:19:30 -0700

    Load review data and hall data from firestore.

--- src/Pages/ExampleHallPage.jsx
+++ src/Pages/ExampleHallPage.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
+import { getReviewsByDormName, getDormDocs } from '../firestore'
 import {Container, Row, Col, Carousel} from 'react-bootstrap'
+import LoaderComponent from '../Components/LoaderComponent';
 import ExampleStyles from '../Styles/ExampleHallPage.module.css';
 
 class ExampleHallPage extends Component {
@@ -8,101 +10,127 @@ class ExampleHallPage extends Component {
   constructor(props)
   {
     super(props);
-    this.state = this.props.location.state;
+    this.state = 
+    {
+      hallName: this.props.location.state.hallName,
+      hallReviews: null,
+      hallDocs: null,
+      loaded: false
+    }
 
-    //Get hall name:
-    this.hallName = this.props.location.state.hallName;
   }
 
   componentDidMount()
   {
+    //Loading Hall reviews from database.
+    getReviewsByDormName(this.state.hallName)
+    .then((result)=>
+      { 
+        this.setState({hallReviews: result});
+        this.setState({loaded: true});
+      }
+    );
+
+    //Loading Hall info from database.
+    getDormDocs()
+    .then((result)=>
+      { 
+        this.setState({hallDocs: result});
+      }
+    );
 
   }
 
   render()
   {
+    
+    if(this.state.loaded)
+    {
+      return (
+        <div>
 
-    return(
+          <Container fluid='true' className={ExampleStyles.windowDivSection}>
 
-      <div>
+            <Row>
 
-        <Container fluid='true' className={ExampleStyles.windowDivSection}>
-          
-          <Row>
+              <Col className={ExampleStyles.middleSection}>
 
-            <Col className={ExampleStyles.middleSection}>
+                <Row className={ExampleStyles.infoAndTopReviewSection}>
 
-             <Row className={ExampleStyles.infoAndTopReviewSection}>
+                  <Col className={ExampleStyles.titleAndInformationSection}>
 
-              <Col className={ExampleStyles.titleAndInformationSection}>
-                  
-                <Row className={ExampleStyles.titleBlock}>
-                  <h1>{this.hallName}</h1>
+                    <Row className={ExampleStyles.titleBlock}>
+                      <h1>{this.state.hallName}</h1>
+                    </Row>
+
+                    <Row className={ExampleStyles.imageGalleryBlock}>
+
+                      <Carousel className={ExampleStyles.imageCarousel}>
+                        <Carousel.Item>
+                          <img
+                            className={ExampleStyles.carouselImageExample}
+                            src="https://www.kpq.com/wp-content/uploads/2018/07/CWU.jpg"
+                            alt="First slide"
+                          />
+                        </Carousel.Item>
+
+                        <Carousel.Item>
+                          <img
+                            className={ExampleStyles.carouselImageExample}
+                            src="https://katu.com/resources/media/b2bd1ced-1737-478c-92d9-fa0fc374d6a2-large16x9_190419_pio_central_washington_university_cwu.jpg?1555702482536"
+                            alt="First slide"
+                          />
+                        </Carousel.Item>
+
+                        <Carousel.Item>
+                          <img
+                            className={ExampleStyles.carouselImageExample}
+                            src="https://www.kpq.com/wp-content/uploads/2018/07/CWU.jpg"
+                            alt="First slide"
+                          />
+                        </Carousel.Item>
+                      </Carousel>
+
+                    </Row>
+
+                    <Row className={ExampleStyles.infoBlock}>
+                      <h1>{this.state.hallReviews[0].review}</h1>
+                    </Row>
+
+                  </Col>
+
+                  <Col className={ExampleStyles.topReviewSection}>
+
+                    <Row className={ExampleStyles.topReviewBlock}>
+                      <h1>Top Reviews Here</h1>
+                    </Row>
+
+                  </Col>
+
                 </Row>
 
-                  <Row className={ExampleStyles.imageGalleryBlock}>
+                <Row className={ExampleStyles.reviewsSection}>
 
-                    <Carousel className={ExampleStyles.imageCarousel}>
-                      <Carousel.Item>
-                        <img
-                          className={ExampleStyles.carouselImageExample}
-                          src="https://www.kpq.com/wp-content/uploads/2018/07/CWU.jpg"
-                          alt="First slide"
-                        />
-                      </Carousel.Item>
+                  <div className={ExampleStyles.reviewsBlock}>
+                    <h1>Reviews</h1>
+                  </div>
 
-                      <Carousel.Item>
-                        <img
-                          className={ExampleStyles.carouselImageExample}
-                          src="https://katu.com/resources/media/b2bd1ced-1737-478c-92d9-fa0fc374d6a2-large16x9_190419_pio_central_washington_university_cwu.jpg?1555702482536"
-                          alt="First slide"
-                        />
-                      </Carousel.Item>
-
-                      <Carousel.Item>
-                        <img
-                          className={ExampleStyles.carouselImageExample}
-                          src="https://www.kpq.com/wp-content/uploads/2018/07/CWU.jpg"
-                          alt="First slide"
-                        />
-                      </Carousel.Item>
-                    </Carousel>
-
-                  </Row>
-
-                <Row className={ExampleStyles.infoBlock}>
-                  <h1>Info</h1>
                 </Row>
 
               </Col>
 
-              <Col className={ExampleStyles.topReviewSection}>
+            </Row>
 
-                <Row className={ExampleStyles.topReviewBlock}>
-                  <h1>Top Reviews Here</h1>
-                </Row>
+          </Container>
 
-              </Col>
-
-             </Row>
-
-             <Row className={ExampleStyles.reviewsSection}>
-               
-               <div className={ExampleStyles.reviewsBlock}>
-                  <h1>Reviews</h1>
-               </div>
-
-             </Row>
-
-            </Col>
-
-          </Row>
-
-        </Container>
-
-      </div>
-    )
-
+        </div>
+      )
+    }
+    else //List is still loading. 
+    {
+      return(<LoaderComponent />);
+    }
+    
   }
 
 }
