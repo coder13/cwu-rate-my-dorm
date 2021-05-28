826f6e507b601a0ffd91378b466f1cbe9c5a555f
Elisabeth Jenkins <elisabeth.jenkins@cwu.edu>
2021-05-28 15:37:02 -0700

    Co-authored-by: JJSteiner <JJSteiner@users.noreply.github.com> Co-authored-by: elimccoy <xcylei@users.noreply.github.com>

--- src/Pages/ProfilePage.jsx
+++ src/Pages/ProfilePage.jsx
@@ -108,26 +108,6 @@ const ProfilePage = () => {
       </div>
 
     </div>
-
-
-
-    // <div>
-    //   <div>
-    //     <div
-    //       style={{
-    //         background: `url(${photoURL || 'https://res.cloudinary.com/dqcsk8rsc/image/upload/v1577268053/avatar-1-bitmoji_upgwhc.png'})  no-repeat center center`,
-    //         backgroundSize: "cover",
-    //         height: "200px",
-    //         width: "200px"
-    //       }}
-    //     />
-    //     <div>
-    //       <h2>{displayName}</h2>
-    //       <h3>{email}</h3>
-    //     </div>
-    //   </div>
-    //   <button onClick={() => {auth.signOut()}}>Sign out</button>
-    // </div>
   ) 
 };
 
