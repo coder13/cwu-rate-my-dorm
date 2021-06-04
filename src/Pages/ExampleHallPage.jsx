6f73c5ebc627a23a8f876aa2ec4ede3399cbf403
Elijah <elijmccoy01@gmail.com>
2021-06-04 01:16:43 -0700

    added all of the calculated averages to each page. (Things like bathroom, cleanliness, ect.)

--- src/Pages/ExampleHallPage.jsx
+++ src/Pages/ExampleHallPage.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
-import {getReviewsByDormName, getDormByName} from '../firestore'
+import {getReviewsByDormName, getDormByName, getRatings} from '../firestore'
 import {Container, Row, Carousel, Card, Button} from 'react-bootstrap'
 import LoaderComponent from '../Components/LoaderComponent';
 import XButton from '../Assets/xButton.png'
@@ -19,7 +19,13 @@ class ExampleHallPage extends Component {
       hallDocs: null,
       hallImages: null,
       hallDescription: null,
-      hallRating: null,
+      overallRatingTotal: null,
+      locationRatingTotal: null,
+      roomSizeRatingTotal: null, 
+      furnitureRatingTotal: null,
+      commonAreasRatingTotal: null,
+      cleanlinessRatingTotal: null,
+      bathroomRatingTotal: null,
       currentUserImage: null,
       displayCurUserImage: false,
       loaded: false
@@ -61,6 +67,20 @@ class ExampleHallPage extends Component {
         this.setState({hallImages: dormNameResult.get("images")});
         this.setState({hallDescription: dormNameResult.get("description")});
         this.setState({hallRating: dormNameResult.get("rating")});
+        this.setState({loaded: true});
+      });
+
+    }).then(()=>{
+
+      getRatings(this.state.hallName)
+      .then((dormResult) => {
+        this.setState({overallRatingTotal: dormResult[0]});
+        this.setState({locationRatingTotal: dormResult[1]});
+        this.setState({roomSizeRatingTotal: dormResult[2]});
+        this.setState({furnitureRatingTotal: dormResult[3]});
+        this.setState({commonAreasRatingTotal: dormResult[4]});
+        this.setState({cleanlinessRatingTotal: dormResult[5]});
+        this.setState({bathroomRatingTotal: dormResult[6]});
         this.setState({loaded: true});
       });
 
@@ -228,14 +248,30 @@ class ExampleHallPage extends Component {
                       <Card bg={'light'} className={ExampleStyles.infoCard}>
                         <Card.Header><b>Hall Information:</b></Card.Header>
                         <Card.Body>
+                          
+                          <div className={ExampleStyles.infoCardOverall}>
+                            Average Hall Rating: {this.state.overallRatingTotal}
+                          </div>
+
+                          <div className={ExampleStyles.infoCardSub}>
+                            <p>
+                              Location Rating: {this.state.locationRatingTotal}<br/>
+                              Room Size Rating: {this.state.roomSizeRatingTotal}<br/>
+                              Furiture Rating: {this.state.furnitureRatingTotal}<br/>
+                              Common Areas Rating: {this.state.commonAreasRatingTotal}<br/>
+                              Cleanliness Rating: {this.state.cleanlinessRatingTotal}<br/>
+                              Bathroom Rating: {this.state.bathroomRatingTotal}<br/>
+                            </p>
+                          </div>
+
                           <Card.Text>
-                            <h3>Average Hall Rating: {this.state.hallRating}</h3>
-                            <br />
+
                             <h5>Description:</h5>
                             {this.state.hallDescription}
                             <br />
                             <br />
                             <Button variant="primary" onClick={() => { this.navigateToPage(this.state.hallName) }}>More Info</Button>
+
                           </Card.Text>
                         </Card.Body>
                       </Card>
